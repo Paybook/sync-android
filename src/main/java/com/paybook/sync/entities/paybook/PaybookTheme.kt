5367ae1f842df8f2fc8b6088b5c28ef9fd52fe9f
Gerardo Teruel <gerardogtn@gmail.com>
2018-08-23 19:21:53 -0500

    :star: Add color field

--- src/main/java/com/paybook/sync/entities/paybook/PaybookTheme.kt
+++ src/main/java/com/paybook/sync/entities/paybook/PaybookTheme.kt
@@ -1,5 +1,6 @@
 package com.paybook.sync.entities.paybook
 
+import android.support.annotation.ColorInt
 import java.io.Serializable
 import java.math.BigDecimal
 
@@ -11,5 +12,6 @@ data class PaybookTheme(
   val id: String,
   val name: String,
   val icon: String,
-  val balance: BigDecimal
+  val balance: BigDecimal,
+  @ColorInt val color: Int
 ) : Serializable
